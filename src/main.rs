a939e57de9ce4caa851d68a1cf86106ceb069595
Stephen Akuffo <sa.akuffo@gmail.com>
2023-01-27 12:54:25 -0500

    systems built

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
+mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -12,8 +15,11 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
 }
 
 use prelude::*;
@@ -30,6 +36,7 @@ impl State {
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
@@ -40,18 +47,15 @@ impl State {
     }
 }
 
-pub fn build_scheduler() -> Schedule {
-    Schedule::builder().build()
-}
-
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(0);
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        // TODO Execute systems
-        // TODO Render Draw Buffer
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
@@ -69,3 +73,11 @@ fn main() -> BError {
 
     main_loop(context, State::new())
 }
+
+// Hands-on Rust Book Notes
+/*
+   We are going ot need 3 systems
+   1. an input handler, I am guessing the keyboard interface/logic
+   2. a map renderer, self explanatory but renders the map i would assume
+   3. an entity renderer, renders the entities on the map
+*/
